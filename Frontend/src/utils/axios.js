aec0565972747640ff962ef4a282983dcfb39460
Rohit-student584 <bwubts23530@brainwareuniversity.ac.in>
2026-06-27 19:23:22 +0530

    Production API fix

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,25 +1,18 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
-
     (config) => {
-
         const token = localStorage.getItem("token");
 
         if (token) {
-
             config.headers.Authorization = `Bearer ${token}`;
-
         }
 
         return config;
-
     },
-
     (error) => Promise.reject(error)
-
 );
